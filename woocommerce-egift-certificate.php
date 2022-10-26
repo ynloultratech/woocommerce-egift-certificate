d61d73a1dc5ae1094cbab829b065c649de509159
Rafael Santos <raf.rsr@gmail.com>
2022-10-26 16:55:27 -0400

    dump 1.0.5

--- woocommerce-egift-certificate.php
+++ woocommerce-egift-certificate.php
@@ -13,7 +13,7 @@
  * Plugin Name: WooCommerce eGiftCertificate
  * Plugin URI: https://www.paynup.com
  * Description: Use eGiftCertificate as a form of exchange for goods
- * Version: 1.0.4
+ * Version: 1.0.5
  * Author: YnloUltratech
  * Author URI: http://ynloultratech.com
  * Requires PHP: 5.6
